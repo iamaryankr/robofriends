075408c77356612f4dfa286fb9dddaeccfd9d506
iamaryankr <95643709+iamaryankr@users.noreply.github.com>
2024-05-27 03:12:14 +0530

    first react

---
+++ src/containers/App.js
@@ -0,0 +1,54 @@
+import React, { Component } from 'react';
+import CardList from '../components/CardList';
+import {robots} from '../robots';
+import SearchBox from '../components/SearchBox';
+import './App.css';
+import Scroll from '../components/Scroll';
+
+//STATE? something that can change and effect our app
+
+class App extends React.Component {
+    constructor(){
+        super()
+        this.state = {
+            robots: [], 
+            searchfield: ''
+        }
+    }
+
+    componentDidMount() {
+        fetch('https://jsonplaceholder.typicode.com/users').then(response => {
+            response.json();
+        })
+        .then(users => {
+            this.setState({robots: robots})
+        });
+    }
+
+    onSearchChange = (event) => {
+        this.setState({searchfield: event.target.value});
+    }
+
+    render(){
+        const filteredRobots = this.state.robots.filter(robot => {
+            return robot.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
+        })
+        if(this.state.robots.length ===0){
+            return <h1>Loading!</h1>
+        }
+        else{
+            return (
+                <div className="tc">
+                    <h1 className="f1">Robofriends</h1>
+                    <SearchBox searchChange = {this.onSearchChange}/>
+                    <Scroll>
+                        <CardList robots={filteredRobots}/>
+                    </Scroll>
+                    
+                </div>
+            );
+        }
+    }
+}
+
+export default App;
